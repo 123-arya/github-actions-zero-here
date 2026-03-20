1843d7b9fe1fb7175774f91d218f8baaebb9b4c8
123-arya <aryaparab08@gmail.com>
2026-03-20 17:26:50 +0530

    Fix remove the debug

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template
-
-
 app = Flask(__name__)
 
 
@@ -9,5 +7,4 @@ def home():
     return render_template("index.html")
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8001)
+app.run(host= '0.0.0.0', port=8001)

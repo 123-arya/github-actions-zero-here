ac41a135767966b85ca9f85ae46c694e401d1479
123-arya <aryaparab08@gmail.com>
2026-03-25 12:22:30 +0530

    Update thefile

--- app.py
+++ app.py
@@ -6,9 +6,4 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-
-<<<<<<< HEAD
-app.run(port=8001)
-=======
-app.run(host='0.0.0.0', port=8001)
->>>>>>> c19934c5a5879568bf0cf8f8c3a9cf697768d5ff
+app.run(port= 8081)
